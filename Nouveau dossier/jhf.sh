aed94c59898badb1327798d5f1ef81a834e117aa
“khevin” <khevinrabearison@gmail.com>
2024-02-22 07:40:09 +0100

    nanao automatisation

---
+++ Nouveau dossier/jhf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clonehttps://github.com/rabearisonkhevin15/rabearison.git
+cd RABEARISON
+touch file.txt
+mkdir boay kely manohatra
+git add .
+git commit -m "nanao automatisation"
+git push 
+
